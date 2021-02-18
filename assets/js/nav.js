bcd91f51c8b2f921eba2c9ea23ff35ab835b6d23
Shivansh-J <75092730+Shivansh-J@users.noreply.github.com>
2021-02-18 12:38:25 +0530

    Update nav.js

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,1 +1,15 @@
+function openServices() {
+            document.getElementsByClassName("v-nav-services")[0].style.left = "0px";
+        }
 
+        function closeServices() {
+            document.getElementsByClassName("v-nav-services")[0].style.left = "-15rem";
+        }
+
+        function openNav() {
+            document.getElementsByClassName("v-nav-primary")[0].style.left = "0px";
+        }
+
+        function closeNav() {
+            document.getElementsByClassName("v-nav-primary")[0].style.left = "-15rem";
+        }
