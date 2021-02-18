da2370b0ddd095afadbdcb4258b196d22ddae4c2
Adarsh <46349391+4d4r5h@users.noreply.github.com>
2021-02-18 13:47:02 +0530

    Update and rename map.html to map.php

--- map.php
+++ map.php
@@ -1,3 +1,7 @@
+<?php
+include 'dbconnect.php';
+session_start();
+?>
 <!doctype html>
 <html>
 	<head>
@@ -5,10 +9,10 @@
 		 <meta name="viewport"
 			content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
-		<title>Thak gya hoon</title>
+		<title>Agricultural Land</title>
 		<link rel="stylesheet" href="assets/css/reset.css">
 		<link rel="stylesheet" href="assets/css/map-styles.css">
-		<link rel="stylesheet" href="assets/css/nav.css">
+		<link rel="stylesheet" href="assets/css/nav.php">
 		<style>
 			path[title="Uttar Pradesh"] {
 				fill: #558B2F;
