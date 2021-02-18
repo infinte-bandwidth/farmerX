c1dc4c3d1625f1561709fc433dce66b7ffb28582
Adarsh <46349391+4d4r5h@users.noreply.github.com>
2021-02-18 14:17:10 +0530

    Add files via upload

---
+++ dbconnect.php
@@ -0,0 +1,12 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "users";
+
+// connecting to database
+$conn = mysqli_connect($server,$username,$password,$database);
+if(!$conn)
+{
+    die("Error".mysqli_connect_error());
+}
