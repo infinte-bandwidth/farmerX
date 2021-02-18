a0bcbf8daaa0ccf297ea2acc211d6ac0170cb9a9
Shivansh-J <75092730+Shivansh-J@users.noreply.github.com>
2021-02-18 12:50:58 +0530

    Update news.php

--- news.php
+++ news.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-    <link rel="stylesheet" href="styles/nav.css">
+    <link rel="stylesheet" href="styles/nav.php">
 	<link rel="stylesheet" href="assets/css/reset.css"> <!--CSS RESET FOR CONSISTANCE LOOK ON BROWSERS-->
 	<link rel="stylesheet" href="assets/css/news.css">
 
