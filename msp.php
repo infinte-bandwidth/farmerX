4daab97a36ed5460606602daab46db1806a21fef
Shivansh-J <75092730+Shivansh-J@users.noreply.github.com>
2021-02-19 19:26:35 +0530

    Update msp.php

--- msp.php
+++ msp.php
@@ -12,10 +12,85 @@ include 'dbconnect.php';
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="//cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="assets/css/nav.php">
+	<link rel="stylesheet" href="assets/css/reset.css">
 </head>
 
 <body>
-
+  <nav class="h-nav">
+        <ion-icon name="menu-outline" onclick="openNav()"></ion-icon>
+        <?php
+        if (!isset($_SESSION['loggedin'])) {
+            echo "<a href='login.php' class='login'><ion-icon name='log-in-outline'></ion-icon></a>
+	        <a href='signup.php' class='sign-up'><ion-icon name='person-add-outline'></ion-icon></a>";
+        } else {
+            echo "<a href='logout.php' class='login'><ion-icon name='log-out-outline'></ion-icon></a>";
+        }
+        ?>
+    </nav>
+    <nav class="v-nav-primary">
+        <ion-icon name="return-up-back-outline" onclick="closeNav()"></ion-icon>
+        <a class="item1">
+            <?php
+            if (isset($_SESSION['username'])) {
+                echo '<ion-icon name=""></ion-icon>';
+                echo $_SESSION['username'];
+            } else {
+                echo '<ion-icon name="leaf-outline"></ion-icon>';
+                echo "Menu";
+            }
+            ?>
+        </a>
+        <a href="index.php" class="v-nav-item item2">
+            <ion-icon name="home-outline"></ion-icon>
+            Home
+        </a>
+        <a href="javascript:openServices()" class="v-nav-item">
+            <ion-icon name="bar-chart-outline"></ion-icon>
+            Services
+        </a>
+        <a href="" class="v-nav-item">
+            <ion-icon name="call-outline"></ion-icon>
+            Contact Us
+        </a>
+    </nav>
+    <nav class="v-nav-services">
+        <ion-icon name="return-up-back-outline" onclick="closeServices()"></ion-icon>
+        <a class="item1">
+            <?php
+            if (isset($_SESSION['username'])) {
+                echo "<ion-icon name=''></ion-icon>";
+            } else {
+                echo "<ion-icon name='bag-handle'></ion-icon>";
+            }
+            ?>
+            Services
+        </a>
+        <a href="weather.php" class="v-nav-item">
+            <ion-icon name="cloudy-night-outline"></ion-icon>
+            Weather Forcast
+        </a>
+        <a href="resources.php" class="v-nav-item">
+            <ion-icon name="bag-check-outline"></ion-icon>
+            Resources
+        </a>
+        <a href="msp.php" class="v-nav-item">
+            <ion-icon name="logo-usd"></ion-icon>
+            MSP
+        </a>
+        <a href="market.php" class="v-nav-item">
+            <ion-icon name="fish-outline"></ion-icon>
+            Market
+        </a>
+        <a href="news.php" class="v-nav-item">
+            <ion-icon name="color-fill-outline"></ion-icon>
+            News
+        </a>
+        <a href="map.php" class="v-nav-item">
+            <ion-icon name="finger-print-outline"></ion-icon>
+            Agricultural Land
+        </a>
+    </nav>
 
     <div class="container">
         <h1 style="text-align: center;font-family: 'Roboto Mono', monospace;">Minimum Support Prices</h1>
@@ -63,10 +138,30 @@ include 'dbconnect.php';
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
 
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+   
     <script src="//cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script src="assets/js/jquery-2.1.0.min.js"></script>
+
+<!-- Bootstrap -->
+<script src="assets/js/popper.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+
+<!-- Plugins -->
+<script src="assets/js/owl-carousel.js"></script>
+<script src="assets/js/scrollreveal.min.js"></script>
+<script src="assets/js/waypoints.min.js"></script>
+<script src="assets/js/jquery.counterup.min.js"></script>
+<script src="assets/js/imgfix.min.js"></script>
+
+<!-- Global Init -->
+<script src="assets/js/custom.js"></script>
+<script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
+<script src="assets/js/nav.js"></script>
 
     <script>
         $(document).ready(function() {
