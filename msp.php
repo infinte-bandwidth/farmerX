1e31bb9854871597b9ece64bb6f3e70700bf94bf
Manav Gondalia <72291135+manavgondalia@users.noreply.github.com>
2021-02-19 16:40:33 +0530

    Update msp.php

--- msp.php
+++ msp.php
@@ -1,8 +1,9 @@
 <?php
-   include 'dbconnect.php';
+include 'dbconnect.php';
 ?>
 <!doctype html>
 <html lang="en">
+
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -15,42 +16,45 @@
 
 <body>
 
-<div class="container">
-<table class="table" id="myTable">
 
-    <thead>
-    <tr>
-      <th scope="col">S. No.</th>
-      <th scope="col">Crops</th>
-      <th scope="col">2017-18</th>
-      <th scope="col">2018-19</th>
-      <th scope="col">2019-20</th>
-      <th scope="col">2020-21</th>
-    </tr>
-    </thead>
+    <div class="container">
+        <h1 style="text-align: center;font-family: 'Roboto Mono', monospace;">Minimum Support Prices</h1>
+        <h2 style="text-align:center;font-family: 'Roboto Mono', monospace;">Statement Showing Minimum Support Prices - Fixed by Government (Rs.quintal)</h2>
+        <table class="table table-striped table-dark table-bordered " id="myTable">
 
-    <tbody>
-      <?php
-        $sql ="SELECT * FROM `msp`";
-        $result = mysqli_query($conn, $sql);
-        $sno = 0;
-        while($row = mysqli_fetch_assoc($result)){
-          $sno = $sno + 1; 
-          echo "<tr>
-          <th scope='row'>".$sno."</th>
-          <td>".$row['crop']."</td>
-          <td>".$row['2017-18']."</td>
-          <td>".$row['2018-19']."</td>
-          <td>".$row['2019-20']."</td>
-          <td>".$row['2020-21']."</td></tr>";
-        }
-      ?>
-    </tbody>
+            <thead>
+                <tr>
+                    <th scope="col">S. No.</th>
+                    <th scope="col">Crops</th>
+                    <th scope="col">2017-18</th>
+                    <th scope="col">2018-19</th>
+                    <th scope="col">2019-20</th>
+                    <th scope="col">2020-21</th>
+                </tr>
+            </thead>
 
-</table>
-</div>
+            <tbody>
+                <?php
+                $sql = "SELECT * FROM `msp`";
+                $result = mysqli_query($conn, $sql);
+                $sno = 0;
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $sno = $sno + 1;
+                    echo "<tr>
+          <th scope='row'>" . $sno . "</th>
+          <td>" . $row['crop'] . "</td>
+          <td>" . $row['2017-18'] . "</td>
+          <td>" . $row['2018-19'] . "</td>
+          <td>" . $row['2019-20'] . "</td>
+          <td>" . $row['2020-21'] . "</td></tr>";
+                }
+                ?>
+            </tbody>
 
-<hr>
+        </table>
+    </div>
+
+    <hr>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
@@ -59,22 +63,17 @@
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
 
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-    integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-    crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-    crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-    crossorigin="anonymous"></script>
-  <script src="//cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="//cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
 
     <script>
-       $(document).ready( function () {
-       $('#myTable').DataTable();
-       } );
+        $(document).ready(function() {
+            $('#myTable').DataTable();
+        });
     </script>
 
 </body>
+
 </html>
