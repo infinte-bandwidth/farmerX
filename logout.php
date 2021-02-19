cca1d46b66f8778481c6c2403beccdfcd881e3a0
Adarsh <46349391+4d4r5h@users.noreply.github.com>
2021-02-19 12:44:12 +0530

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,9 @@
+<?php
+    error_reporting(0);
+    session_start();
+    session_unset();
+    session_destroy();
+
+    echo "<script> window.alert('You have been logged out.'); 
+        window.location='index.php'; </script>";
+ ?>
