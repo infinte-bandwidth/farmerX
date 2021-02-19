d7702fbf77dc1c452f6b3ee99595b76ee2b7b382
Shivansh-J <75092730+Shivansh-J@users.noreply.github.com>
2021-02-19 14:49:22 +0530

    Create resources.js

---
+++ assets/js/resources.js
@@ -0,0 +1,49 @@
+var tiles=document.getElementsByClassName("tile");
+	tiles[0].addEventListener("click",function() {
+		if(document.getElementsByClassName("overlay")[0].style.top=="0px") {
+			document.getElementsByClassName("overlay")[0].style.top="95%";
+		} else {
+			document.getElementsByClassName("overlay")[0].style.top="0px";
+		}
+			
+	});
+	tiles[1].addEventListener("click",function() {
+		if(document.getElementsByClassName("overlay")[1].style.top=="0px") {
+			document.getElementsByClassName("overlay")[1].style.top="95%";
+		} else {
+			document.getElementsByClassName("overlay")[1].style.top="0px";
+		}
+			
+	});
+	tiles[2].addEventListener("click",function() {
+		if(document.getElementsByClassName("overlay")[2].style.top=="0px") {
+			document.getElementsByClassName("overlay")[2].style.top="95%";
+		} else {
+			document.getElementsByClassName("overlay")[2].style.top="0px";
+		}
+			
+	});
+	tiles[3].addEventListener("click",function() {
+		if(document.getElementsByClassName("overlay")[3].style.top=="0px") {
+			document.getElementsByClassName("overlay")[3].style.top="95%";
+		} else {
+			document.getElementsByClassName("overlay")[3].style.top="0px";
+		}
+			
+	});
+	tiles[4].addEventListener("click",function() {
+		if(document.getElementsByClassName("overlay")[4].style.top=="0px") {
+			document.getElementsByClassName("overlay")[4].style.top="95%";
+		} else {
+			document.getElementsByClassName("overlay")[4].style.top="0px";
+		}
+			
+	});
+	tiles[5].addEventListener("click",function() {
+		if(document.getElementsByClassName("overlay")[5].style.top=="0px") {
+			document.getElementsByClassName("overlay")[5].style.top="95%";
+		} else {
+			document.getElementsByClassName("overlay")[5].style.top="0px";
+		}
+			
+	});
