0669609920549d2e550b094456db0bbe800593de
Shivansh-J <75092730+Shivansh-J@users.noreply.github.com>
2021-02-20 01:33:38 +0530

    Update weather.php

--- weather.php
+++ weather.php
@@ -291,7 +291,6 @@ else
     </nav>
     <!--NAVIGATION BAR ENDS-->
     
-    <div class="wrap" onclick="ifNav()">
     <?php for ($i = 0; $i < $cnt; $i++) { ?>
 
         <?php if ($time[$i] == "9 AM") { ?> <div class="parallax" style="background-image : url('assets/images/day.jpg')"> </div> <?php } ?>
@@ -336,7 +335,6 @@ else
         </div>
 
     <?php } ?>
-	</div>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
